6171eac23091e6be83e3f71741f6afe6fba4ded1
harshanricse <tunuguntlaharsha@gmail.com>
2025-05-18 21:38:15 +0530

    varaibles

---
+++ variables.js
@@ -0,0 +1,26 @@
+let empId = 1234;//integer
+let empRating = 3.4;//float
+
+console.log('Emp Id'+empId);
+console.log('Emp Rating'+empRating);
+
+let message = 'Hello, World';//string
+console.log(message);
+
+let choice = true;
+let outcome = false;
+
+console.log('Choice: '+choice);
+console.log('Outcome: '+outcome);
+
+let empDoj = new Date();
+console.log('Emp Doj: '+empDoj);
+
+let temp = 100;
+temp = 'hello';
+
+let data;
+console.log('Data: '+data);
+
+let newData = null;
+console.log('New Data: '+newData);
